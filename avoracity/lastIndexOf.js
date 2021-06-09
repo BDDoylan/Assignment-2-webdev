21d059e1e30a76b16a39dbe2762ba4338b32078f
Michael Alvarez <michael.alvarez687@myhunter.cuny.edu>
2021-06-09 12:25:08 -0400

    functionality added
    return the last index else return -1

--- avoracity/lastIndexOf.js
+++ avoracity/lastIndexOf.js
@@ -2,10 +2,21 @@
     function to retreive the last index of the list
 */
 
-let lastIndexOf = {
+Array.prototype.lastIndexOf = function (callback) 
+{
 
-
-    // if last element found, return index of last encountered element
-    // else return -1
+    // cycle through the array
+    for (let index = 0; index < this.length; index++)
+    {
+        // if last element found, return index of last encountered element
+        if ( index == this.length - 1)
+        {
+            return index;
+        }
+        // else return -1
+        return -1
+    }
+   
+        
 };
 
